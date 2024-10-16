095642934b3a4ad879ee62289367d2c46a15b56c
bsowmiya=567 <sowzhasai25@gmail.com>
2024-10-16 15:35:44 +0530

    cord changed

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ import addressRouter from './Routers/addressRouter.js'
 
 const app = express()
 const port = process.env.PORT
-app.use(cors({origin:"https://crms-clients.netlify.app"}))
+// app.use(cors({origin:"https://crms-clients.netlify.app"}))
+// Enable CORS for all routes
+app.use(cors({
+    origin: "https://crms-clients.netlify.app", // Specify allowed origin(s) or use "*" for all
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Specify allowed HTTP methods
+    preflightContinue: false, // Pass the CORS preflight request to the next handler
+    optionsSuccessStatus: 204, // Some legacy browsers choke on 204
+  }));
 app.use(express.json())
 app.use('/api',userRouter)
 app.use('/api/address',addressRouter)
